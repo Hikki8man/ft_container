8b852b4aa2efa5526bd4d1dfc0b4764950772d2c
hikki <johan.chevet@hotmail.fr>
2022-04-14 17:09:54 +0200

    pushpush

--- vector.hpp
+++ vector.hpp
@@ -11,15 +11,10 @@ class vector {
 	public:
 		vector() : _data(NULL), _size(0), _capacity(0) {};
 
-		vector(size_type size, const value_type & value) {
+		vector(size_type size, const value_type & value) {//test with size max
 			_size = size;
 			_capacity = size;
-			try {
-				_data = _alloc.allocate(_capacity);
-			}
-			catch (std::exception & e) {
-				std::cout << e.what() << std::endl;
-			}
+			_data = _alloc.allocate(_capacity);
 			for (size_type i = 0; i < size; i++)
 				_data[i] = value;
 		};
@@ -36,36 +31,37 @@ class vector {
 			return _data[i]; //no protection askip
 		}
 
-		size_type size() const {
+		size_type 	size() const {
 			return _size;
 		}
 		size_type	capacity() const {
 			return _capacity;
 		}
 
+		size_type	max_size() const {
+			return _alloc.max_size();
+		}
+
 		void reserve (size_type n) { //throw exception if neg
 			if (n > _capacity) {
 				value_type *newData;
-				try {
-					newData = _alloc.allocate(n);//max size et tt
-					_capacity = n;
-				}
-				catch (std::exception & e) {
-					std::cout << e.what() << std::endl;
-					return ;
-				}
+				newData = _alloc.allocate(n);//max size et tt
+				_capacity = n;
 				_copy_array(newData, _size);
 			}
 		}
 
-		//if empty ?
 		void resize (size_type n, value_type val = value_type()) {
-			// reserve(n);
-			(void)val;
 			if (n < _size) {
 				_size = n;
 			}
-
+			else if (n > _size) {
+				_adjust_capacity(n);
+				while (_size < n) {
+					_data[_size] = val;
+					++_size;
+				}
+			}
 		}
 
 		bool empty() const {
@@ -73,19 +69,7 @@ class vector {
 		}
 
 		void	push_back(const value_type& val) {
-			size_type toDealloc = _capacity;
-			if (_size + 1 > _capacity) {
-				_adjust_capacity(_size + 1);
-				value_type *newData;
-				try {
-					newData = _alloc.allocate(_capacity);
-				}
-				catch (std::exception & e) {
-					std::cout << e.what() << std::endl;
-					return ;
-				}
-				_copy_array(newData, toDealloc);
-			}
+			_adjust_capacity(_size + 1);
 			_data[_size] = val;
 			++_size;
 		}
@@ -101,22 +85,20 @@ class vector {
 				newData[i] = _data[i];
 			}
 			if (toDealloc)
-			{
-				try {
-					_alloc.deallocate(_data, toDealloc);
-				}
-				catch (std::exception & e) {
-					std::cout << e.what() << std::endl;
-				}
-			}
+				_alloc.deallocate(_data, toDealloc);
 			_data = newData;
 		}
 		
-		void _adjust_capacity(const size_t & newSize) {
-			if (_capacity == 0)
+		void _adjust_capacity(size_t newSize) {
+			if (newSize > _capacity) {
+				value_type *newData;
+		
+				if (_capacity)
+					newSize = _capacity * 2;
+				newData = _alloc.allocate(newSize);
+				_copy_array(newData, _capacity);
 				_capacity = newSize;
-			else if (newSize > _capacity)
-				_capacity *= 2;
+			}
 		}
 };
 
