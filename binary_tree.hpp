3ec4af51bc37e77c3292a98bcb051e7612fca61b
hikki <johan.chevet@hotmail.fr>
2022-06-08 15:16:44 +0200

    rien ne va

--- binary_tree.hpp
+++ binary_tree.hpp
@@ -2,27 +2,99 @@
 #define BINARY_TREE_HPP
 
 #include <memory>
+// #include "bi_tree_iterator.hpp"
+#include "iterator_traits.hpp"
 
 namespace ft {
 
 	template<class value_type>
-	struct BInode {
-		typedef BInode<value_type>* node_ptr;
+	struct tree_node {
+		typedef tree_node<value_type>* node_ptr;
 
-		BInode(const value_type& value) : value(value), left(NULL), right(NULL), parent(NULL) {}
+		tree_node() : value(), left(NULL), right(NULL), parent(NULL) {}
+
+		tree_node(const value_type& value) : value(value), left(NULL), right(NULL), parent(NULL) {}
 	
 		value_type value;
 		node_ptr left;
 		node_ptr right;
 		node_ptr parent;
+
+		value_type& operator*() { return value; }
+
+		tree_node& operator++() {
+			if (right) {
+				*this = *right;
+			} else {
+				node_ptr p = parent;
+				while (p && p->right == *this) {
+					*this = *p;
+					p = p->parent;
+				}
+				if (p) {
+					*this = *p;
+				}
+			}
+			return *this;
+		}
+	};
+
+template< typename _Iterator >
+	struct bi_tree_iterator {
+
+			_Iterator _node;
+
+			typedef _Iterator value_type;
+			typedef _Iterator& reference;
+			typedef _Iterator* pointer;
+
+			typedef ft::bidirectional_iterator_tag	iterator_category;
+			typedef ptrdiff_t	difference_type;
+
+			typedef bi_tree_iterator<_Iterator> _Self;
+			typedef typename ft::tree_node<value_type>::node_ptr _Base_ptr;
+			typedef bi_tree_iterator<_Iterator>* _Link_type;
+
+			bi_tree_iterator() : _node() {}
+
+			bi_tree_iterator(_Base_ptr _x) : _node(_x) {}
+
+			reference operator*() const { return *_node; }
+
+			pointer operator->() const { return _node; }
+
+			_Self& operator++() {
+				_node++;
+				return *this;
+			}
+
 	};
 
 	template<typename _Key, typename _Val, typename _Alloc = std::allocator<_Val> >
 	class BItree {
 
-		protected:
-			typedef BInode<_Val> node;
+		// protected:
+		public:
+			typedef tree_node<_Val> node;
 			node *_root;
+
+			template< class _node_ptr >
+			_node_ptr _min(_node_ptr _x) {
+				while (_x->left != NULL) {
+					_x = _x->left;
+				}
+				return _x;
+			}
+
+			template< class _node_ptr >
+			_node_ptr _max(_node_ptr _x) {
+				while (_x->right != NULL) {
+					_x = _x->right;
+				}
+				return _x;
+			}
+
+
 
 		public:
 
@@ -34,7 +106,9 @@ namespace ft {
 			typedef value_type* pointer;
 			typedef const value_type* const_pointer;
 
-			BItree() : root(NULL) {}
+			typedef ft::bi_tree_iterator<node*> iterator;
+
+			BItree() : _root(NULL) {}
 
 			void insert(const value_type& val) {
 				if (_root == NULL) {
@@ -67,7 +141,18 @@ namespace ft {
 				}
 			}
 
-	}
+			iterator begin() {
+				return iterator(_min(_root));
+			}
+
+			iterator end() {
+				return iterator(NULL);
+			}
+
+	};
+
+
+	
 }
 
 #endif
--- main_test.cpp
+++ main_test.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <vector>
 #include "map.hpp"
+#include "binary_tree.hpp"
+// #include <pair>
+
+
 
 int main() {
 
@@ -15,6 +19,11 @@ int main() {
 	for (auto it = m.begin(); it != m.end(); ++it) {
 		std::cout << it->first << " " << it->second << std::endl;
 	}
-	
+
+	ft::BItree<int, ft::pair<int, char> > bt;
+	bt.insert(ft::make_pair(1, 'a'));
+	// std::cout << "bt.begin() = " << << std::endl;
+
+	// std::__tree<int, std::pair<int, char>, _Select1st<std::pair<int, char> >, std::less<int>, std::allocator<std::pair<int, char>> >
 	return 0;
 }
